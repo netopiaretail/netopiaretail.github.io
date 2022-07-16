2410a4920cfeb7aa59e693f126559783d550f7fa
Ion <ion@netopia.ro>
2022-07-16 18:29:31 +0300

    some fixes

--- src/wizard/CoolNav.js
+++ src/wizard/CoolNav.js
@@ -2,20 +2,11 @@ import Stats from "./Stats";
 
 const CoolNav = (props) => {
   const steps = [];
-  const currentStep = props.currentStep;
 
   for (let i = 1; i <= props.totalSteps; i++) {
-    const isActive = currentStep === i;
-    const isChecked = currentStep > i;
-
     steps.push(
-      <li
-        className={`col-2 ${isActive ? "active" : isChecked ? "check" : ""}`}
-        key={i}
-      >
-        <div className={`${isActive ? "active" : isChecked ? "check" : ""}`}>
-          {isActive || isChecked ? <i className="fa-solid fa-check" /> : i}
-        </div>
+      <li key={i} className={i === props.currentStep ? "active" : ""}>
+        <div>{i}</div>
       </li>
     );
   }
